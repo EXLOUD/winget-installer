6a431f53e0750d3d249b14bc887224879393fda4
VLADYSLAV BOBER <vlad.rosys29@gmail.com>
2025-07-22 14:29:02 +0300

    📰 Update Launcher.bat

--- Launcher.bat
+++ Launcher.bat
@@ -3,19 +3,13 @@ setlocal enabledelayedexpansion
 :: =========================================================
 :: 1. Self-elevation
 :: =========================================================
->nul 2>&1 "%SYSTEMROOT%\system32\cacls.exe" "%SYSTEMROOT%\system32\config\system"
-if '%errorlevel%' NEQ '0' (
-    echo Requesting elevation...
-    goto UACPrompt
-)
-goto Admin
-:UACPrompt
-    echo Set UAC = CreateObject^("Shell.Application"^) > "%temp%\getadmin.vbs"
-    echo UAC.ShellExecute "%~s0", "", "", "runas", 1 >> "%temp%\getadmin.vbs"
-    "%temp%\getadmin.vbs"
-    del "%temp%\getadmin.vbs"
-    exit /B
-:Admin
+if "%1"=="admin" goto :AdminMode
+
+echo [INFO] Launch with admin rights...
+"%SystemDrive%\Windows\System32\WindowsPowerShell\v1.0\powershell.exe" -Command "Start-Process cmd -ArgumentList '/c \"%~f0\" admin' -Verb RunAs"
+exit /B
+
+:AdminMode
 pushd "%CD%"
 CD /D "%~dp0"
 :: =========================================================
